a7e5d2e5466e77ce95e733d6ec179c1a9754bf88
Sergio Cabral <git@sergiocabral.com>
2022-01-13 02:24:17 -0300

    Adicionados mensagens de log no backend

--- index.js
+++ index.js
@@ -17,33 +17,71 @@ class App {
     }
 
     async __loadData() {
+        const url = 'https://api2.binance.com/api/v3/ticker/24hr';
         const baseCoin = 'BUSD';
-        const response = await axios.get('https://api2.binance.com/api/v3/ticker/24hr');
-        this.__coins = response.data
-            .filter(coin => 
-                coin.lastPrice > 0 && (
-                    coin.symbol.startsWith(baseCoin) ||
-                    coin.symbol.endsWith(baseCoin)
+
+        console.log(`Carregando dados da API da Binance em ${url}.`);
+        try {
+            const response = await axios.get(url);
+            console.debug(`Recebido dados da API da Binance. Status: ${response.status}, ${response.statusText}. Dados: ${JSON.stringify(response.data)}`);
+            this.__coins = response.data
+                .filter(coin => 
+                    coin.lastPrice > 0 && (
+                        coin.symbol.startsWith(baseCoin) ||
+                        coin.symbol.endsWith(baseCoin)
+                    )
                 )
-            )
-            .map(coin => ({
-                symbol: coin.symbol.replace(baseCoin, '').trim().toUpperCase(),
-                price: coin.symbol.startsWith(baseCoin) ? 1 / coin.lastPrice : Number(coin.lastPrice),
-            }))
-            .concat([{
-                symbol: baseCoin,
-                price: 1,
-                baseCoin: true
-            }])
-            .sort((a, b) => a.symbol.localeCompare(b.symbol));
+                .map(coin => ({
+                    symbol: coin.symbol.replace(baseCoin, '').trim().toUpperCase(),
+                    price: coin.symbol.startsWith(baseCoin) ? 1 / coin.lastPrice : Number(coin.lastPrice),
+                }))
+                .concat([{
+                    symbol: baseCoin,
+                    price: 1,
+                    baseCoin: true
+                }])
+                .sort((a, b) => a.symbol.localeCompare(b.symbol));
+                console.debug(`A lista de moedas foi atualizada com ${this.__coins.length} itens: ${this.__coins.map(coin => coin.symbol).join(', ')}`);
+            } catch (error) {
+            console.error(`Ocorreu um erro ao carregar dados da API da Binance: ${error?.message ?? error}`);
+        }
     }
 
     __middleware() {
         this.__express.use(express.json());
-        this.__express.use(express.static(path.join(__dirname, 'wwwroot')));
+
+        const wwwroot = path.join(__dirname, 'wwwroot');
+        this.__express.use(express.static(wwwroot));
+        console.log(`Aplicação está usando a pasta ${wwwroot} como wwwroot.`);
+
+        this.__express.use((req, res, next) => {
+            res.on('finish', () => {
+                if (res.statusCode >= 400) {
+                    console.warn(`Resposta ${res.statusCode} para ${req.method} ${req.url}`);
+                } else {
+                    console.debug(`Resposta ${res.statusCode} para ${req.method} ${req.url}`);
+                }
+            });
+            next();
+        });
     }
 
     __routes() {
+        this.__express.post('/api/log/:key', async (req, res) => {
+            switch (req.params.key) {
+                case 'frontpage':
+                    console.log(`Página inicial carregada por um usuário.`);
+                    break;
+                case 'ping':
+                    console.debug(`Usuário pingou o servidor.`);
+                    break;
+                default:
+                    console.warn(`Recebido evento de log deconhecido: ${req.params.key}`);
+                    break;
+            }
+            res.status(200).send();
+        });
+
         this.__express.get('/api/coins', async (req, res) => {
             res.json(this.__availableSymbols);
         });
@@ -54,24 +92,35 @@ class App {
             const ammount = Number(req.params.ammount ?? 1);
 
             if (this.__availableSymbols.length === 0) {
+                console.warn(`Conversão não pode ser realizada pois a lista de moedas está vazia.`);
                 res.status(500).json({ error: 'No coins available' });
             } else if (Number.isNaN(ammount)) {
+                console.warn(`Conversão não pode ser realizada pois o valor da moeda é inválido.`);
                 res.status(400).json({ error: 'Ammount must be a number' });
             } else if (!this.__availableSymbols.includes(from) || !this.__availableSymbols.includes(to)) {
+                console.warn(`Conversão não pode ser realizada pois a moeda ${from} ou ${to} não está disponível.`);
                 res.status(400).json({ error: `Symbol must be one of: ${this.__availableSymbols.join(', ')}` });
             } else {
                 const fromPrice = this.__coins.find(coin => coin.symbol === from).price;
                 const toPrice = this.__coins.find(coin => coin.symbol === to).price;
-                res.json({result: (ammount * fromPrice / toPrice).toFixed(8)});
+                const result = (ammount * fromPrice / toPrice).toFixed(8);
+                console.log(`Convertendo ${ammount} ${from} para ${to} = ${result}`);
+                res.json({result});
             }
         });
     }
 
     start() {
         this.__express.listen(this.__port, () => {
-            console.log(`Server listening on port ${this.__port}`);
+            console.log(`Servidor HTTP iniciado na porta ${this.__port}.`);
         });
     }
 }
 
-new App().start();
+try {
+    console.info('Iniciando a aplicação.');
+    new App().start();
+    console.info('Aplicação finalizada.');
+} catch (error) {
+    console.error(`Um erro não tratado ocorreu: ${error?.message ?? error}`);
+}

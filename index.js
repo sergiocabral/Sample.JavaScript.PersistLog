c56ce9dd8d6c3d5782eb386c2444ca49f883830d
Sergio Cabral <git@sergiocabral.com>
2022-01-13 01:03:46 -0300

    API servindo dados reais da Binance

--- index.js
+++ index.js
@@ -6,8 +6,36 @@ class App {
     constructor() {
         this.__port = process.env.PORT || 3000;
         this.__express = express();
+        this.__coins = [];
         this.__middleware();
         this.__routes();
+        this.__loadData();
+    }
+
+    get __availableSymbols() {
+        return this.__coins.map(coin => coin.symbol);
+    }
+
+    async __loadData() {
+        const baseCoin = 'BUSD';
+        const response = await axios.get('https://api2.binance.com/api/v3/ticker/24hr');
+        this.__coins = response.data
+            .filter(coin => 
+                coin.lastPrice > 0 && (
+                    coin.symbol.startsWith(baseCoin) ||
+                    coin.symbol.endsWith(baseCoin)
+                )
+            )
+            .map(coin => ({
+                symbol: coin.symbol.replace(baseCoin, '').trim().toUpperCase(),
+                price: coin.symbol.startsWith(baseCoin) ? 1 / coin.lastPrice : Number(coin.lastPrice),
+            }))
+            .concat([{
+                symbol: baseCoin,
+                price: 1,
+                baseCoin: true
+            }])
+            .sort((a, b) => a.symbol.localeCompare(b.symbol));
     }
 
     __middleware() {
@@ -17,11 +45,25 @@ class App {
 
     __routes() {
         this.__express.get('/api/coins', async (req, res) => {
-            res.json("Hello World, Coins!");
+            res.json(this.__availableSymbols);
         });
 
         this.__express.get('/api/convert/:from/:ammount?/:to?', async (req, res) => {
-            res.json("Hello World, Convert!");
+            const from = String(req.params.from).toUpperCase().trim();
+            const to = String(req.params.to ?? this.__coins.find(coin => coin.baseCoin).symbol).toUpperCase().trim();
+            const ammount = Number(req.params.ammount ?? 1);
+
+            if (this.__availableSymbols.length === 0) {
+                res.status(500).json({ error: 'No coins available' });
+            } else if (Number.isNaN(ammount)) {
+                res.status(400).json({ error: 'Ammount must be a number' });
+            } else if (!this.__availableSymbols.includes(from) || !this.__availableSymbols.includes(to)) {
+                res.status(400).json({ error: `Symbol must be one of: ${this.__availableSymbols.join(', ')}` });
+            } else {
+                const fromPrice = this.__coins.find(coin => coin.symbol === from).price;
+                const toPrice = this.__coins.find(coin => coin.symbol === to).price;
+                res.json({result: (ammount * fromPrice / toPrice).toFixed(8)});
+            }
         });
     }
 
